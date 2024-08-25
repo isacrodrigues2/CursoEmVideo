d6abd0b7d665927a02b35c62a3800809411a58ef
Isac Rodrigues <isacrodrigues2011@live.com>
2024-08-25 19:41:21 -0300

    exercício 60

--- ex060.py
+++ ex060.py
@@ -1,8 +1,6 @@
 '''Exercício Python 060: Faça um programa que leia um número qualquer e mostre o seu fatorial. Exemplo:
 
 5! = 5 x 4 x 3 x 2 x 1 = 120'''
-
-
 num = int(input('Digite um número para calcular seu fatorial: '))
 c = num
 f = 1
