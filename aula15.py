130544b27f19d444070c0fafc0326c03387e6e6e
Isac Rodrigues <isacrodrigues2011@live.com>
2024-08-27 14:17:40 -0300

    aula 15

---
+++ aula15.py
@@ -0,0 +1,12 @@
+n = s = 0
+while True:
+    n = int(input('Informe um número: '))
+    if n == 999:
+        break
+    s+= n
+# print('A soma vale {}'.format(s))
+print(f'A soma vale {s}') # PYTHON 3.6↑
+
+
+
+
