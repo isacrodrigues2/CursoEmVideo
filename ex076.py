b55567dc3810f5458feec750fd6482e73482a321
Isac Rodrigues <isacrodrigues93@gmail.com>
2024-08-29 09:13:15 -0300

    exercício 76

--- ex076.py
+++ ex076.py
@@ -1,3 +1,6 @@
+'''Exercício  Python 076: Crie um programa que tenha uma tupla única com nomes
+ de produtos e seus respectivos preços, na sequência. No final, mostre uma listagem
+  de preços, organizando os dados em forma tabular.'''
 lista = ("Lápis", 1.75,
             "Borracha", 2.00,
             "Caderno", 15.90,
@@ -7,14 +10,13 @@ lista = ("Lápis", 1.75,
             "Mochila", 120.32,
             "Canetas", 22.30,
             "Livro", 34.90)
-
-print("="*50)
-print("{:^50}".format("LISTAGEM DE PREÇOS"))
-print("="*50)
-
+print("="*40)
+print("{:^40}".format("LISTAGEM DE PREÇOS"))
+print("="*40)
 for item in range (0, len(lista)):
     if item % 2 == 0:
         print(f'{lista[item]:.<30}', end='')
     elif item %2 != 0:
         print(f'R${lista[item]:>7.2f}')
+print("="*40)
 input()
